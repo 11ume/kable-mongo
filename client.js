eb5e8a9da3524769feb11c4ca6c186964bd9140e
11ume <horacioriverods@gmail.com>
2019-12-23 09:03:41 -0300

    changes

--- client.js
+++ client.js
@@ -1,18 +1,19 @@
 const { MongoClient } = require('mongodb')
-const url = 'mongodb://localhost:27017'
+//'mongodb://localhost:27017'
+const url = 'mongodb://admin:Kimagure232@localhost:27017/admin'
 const dbName = 'admin'
 
 MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
     const db = client.db(dbName).admin()
-    db.command({ ping: 1 }).then((data) => {
-        console.log(data)
-        client.close()
-    })
+    // db.command({ ping: 1 }).then((data) => {
+    //     console.log(data)
+    //     client.close()
+    // })
     // db.command({ whatsmyuri: 1 }).then((data) => {
     //     console.log(data)
     // })
 
-    // db.command({ serverStatus: 1 })
+    db.command({ serverStatus: 1 }).then(console.log)
     // db.listDatabases((err, dbs) => {
     //     if (err) return console.error(err)
     //     console.log(dbs)
