1eddab47591c566bbf21f0e7ade22b905a9f6e47
11ume <horacioriverods@gmail.com>
2019-12-26 09:51:41 -0300

    changes

--- classic-client.js
+++ classic-client.js
@@ -1,36 +1,48 @@
+const { networkInterfaces } = require('os')
 const { MongoClient } = require('mongodb')
 const url = 'mongodb://admin:Kimagure232@192.168.0.2:27017/admin'
 // const url = 'mongodb://localhost:27017'
 const dbName = 'admin'
-const wildcard = ['localhost', '0.0.0.0', '127.0.0.1']
 
-MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-    const db = client.db(dbName)
-    // db.collection('oplog.rs', console.log).find({ tailable: true })
-    db.executeDbAdminCommand({ "currentOp": 1, "$all": true }, (err, data) => {
-        // const print = (conn) => console.log(conn.client, conn.connectionId)
-        data.inprog.forEach((d) => {
-            if (d.client) {
-                const ip = d.client.split(':')[0]
-                if (ip !== wildcard) {
-                    console.log(d.client, d.connectionId)
-                }
-            }
+const getIps = () => {
+    const interfaces = networkInterfaces()
+    const locals = ['localhost', '0.0.0.0']
+    let ips = []
+    Object.keys(interfaces)
+        .forEach((i) => {
+            const e = interfaces[i].map((i) => i.address)
+            ips = ips.concat(e)
         })
-    })
 
-    // db.command({ ping: 1 }).then((data) => {
-    //     console.log(data)
-    //     client.close()
-    // })
-    // db.command({ whatsmyuri: 1 }).then((data) => {
-    //     console.log(data)
-    // })
+    return ips.concat(locals)
+}
 
-    // db.command({ serverStatus: 1 }).then(console.log)
-    // db.listDatabases((err, dbs) => {
-    //     if (err) return console.error(err)
-    //     console.log(dbs)
-    //     // client.close()
-    // })
-})
+console.log(getIps())
+// MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+//     const db = client.db(dbName)
+//     // db.collection('oplog.rs', console.log).find({ tailable: true })
+//     db.executeDbAdminCommand({ "currentOp": 1, "$all": true }, (err, data) => {
+//         // const print = (conn) => console.log(conn.client, conn.connectionId)
+//         data.inprog.forEach((d) => {
+//             if (d.client) {
+//                 const ip = d.client.split(':')[0]
+//                 console.log(d.client, d.connectionId)
+//             }
+//         })
+//     })
+
+//     // db.command({ ping: 1 }).then((data) => {
+//     //     console.log(data)
+//     //     client.close()
+//     // })
+//     // db.command({ whatsmyuri: 1 }).then((data) => {
+//     //     console.log(data)
+//     // })
+
+//     // db.command({ serverStatus: 1 }).then(console.log)
+//     // db.listDatabases((err, dbs) => {
+//     //     if (err) return console.error(err)
+//     //     console.log(dbs)
+//     //     // client.close()
+//     // })
+// })
