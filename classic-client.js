0723f39b455b1f250307f60bf885a406dd61225d
11ume <horacioriverods@gmail.com>
2019-12-25 21:10:00 -0300

    changes

--- classic-client.js
+++ classic-client.js
@@ -6,7 +6,17 @@ const dbName = 'admin'
 MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
     const db = client.db(dbName)
     // db.collection('oplog.rs', console.log).find({ tailable: true })
-    db.collection('oplog.rs', console.log).watch()
+    // db.collection('$cmd', (err, data) => {
+    //     if (err) return console.error(err)
+    //     console.log(data)
+    // }).watch()
+
+    db.executeDbAdminCommand({ "currentOp": 1, "$all": true }, (err, data) => {
+        const print = (conn) => console.log(conn.client, conn.connectionId)
+        data.inprog
+            .filter((d) => d.client)
+            .forEach(print)
+    })
 
     // db.command({ ping: 1 }).then((data) => {
     //     console.log(data)
