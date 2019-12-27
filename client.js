1c29d04173421c6ea727b1e1b468ab361df034e6
hume <hume@hume@23.org>
2019-12-27 12:53:36 -0300

    improvements

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 const arg = require('arg')
 const kable = require('kable')
 const uriParser = require('./node_modules/mongodb/lib/core/uri_parser')
-const { MongoClient } = require('mongodb')
+const client = require('mongodb').MongoClient
 
 const args = arg({
     '--uri': String
@@ -12,22 +12,14 @@ const args = arg({
     , '-k': '--key'
 })
 
-const connect = ({ uri, client, options }, callback) => {
-    client.connect(uri, options, (err, conn) => {
-        if (err) return callback(err, null)
-        conn.on('serverClosed', () => callback(null, 'closed'))
-        conn.on('serverHeartbeatSucceeded', () => callback(null, 'succeeded'))
-        callback(null, 'connected')
-    })
-}
+const uri = 'mongodb://admin:Kimagure232@192.168.0.2:27017/admin' || args['--uri']
+const options = { useUnifiedTopology: true }
 
-// se puede intentar connectar y ya estar connectado
-const start = ({ uri, id = 'mongo', key = null }) => {
-    const options = { useUnifiedTopology: true }
+const parseUri = (uriIn, opts) => {
     let host = ''
     let port = 0
 
-    uriParser(uri, options, (err, args) => {
+    uriParser(uriIn, opts, (err, args) => {
         if (err) throw err
         if (args.hosts && args.hosts[0]) {
             const address = args.hosts[0]
@@ -36,35 +28,40 @@ const start = ({ uri, id = 'mongo', key = null }) => {
         }
     })
 
-    const k = kable(id, { host, port, key })
-    const connectArgs = { client: MongoClient, uri, options }
-    const call = (err, event) => {
-        if (event === 'succeeded') {
-            if (k.state !== 'UP') {
-                k.start()
-            }
-            return
-        }
-
-        if (err) {
-            k.stop()
-            connect(connectArgs, call)
-            return
-        }
-
-        if (event === 'closed') {
-            k.stop()
-            return
-        }
-
-        k.start()
+    return {
+        host
+        , port
     }
+}
 
-    connect(connectArgs, call)
+const onSucces = (k) => k.state !== 'UP' && k.start()
+
+const onClosed = (k) => k.stop()
+
+const onConnect = (k) => k.start()
+
+const onError = (k, conn, _err) => {
+    // add stop reason to kable
+    k.stop()
+    conn(k, uri, options)
+}
+
+const connect = (k, uri, options) => {
+    client.connect(uri, options, (err, conn) => {
+        if (err) return onError(k, connect, err)
+        conn.on('serverClosed', () => onClosed(k))
+        conn.on('serverHeartbeatSucceeded', () => onSucces(k))
+        onConnect(k)
+    })
+}
+
+const start = ({ id = 'mongo', key = null }) => {
+    const { host, port } = parseUri(uri, options)
+    const k = kable(id, { host, port, key })
+    connect(k, uri, options)
 }
 
 start({
-    uri: 'mongodb://admin:Kimagure232@192.168.0.2:27017/admin' || args['--uri']
-    , id: args['--id']
+    id: args['--id']
     , key: args['--key']
 })
