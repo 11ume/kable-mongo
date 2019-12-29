568789e973cbfecfb7d154f901244ffa089c1f7a
11ume <horacioriverods@gmail.com>
2019-12-29 19:15:41 -0300

    las imrpovements

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const kable = require('kable')
 const client = require('mongodb').MongoClient
 const uriParser = require('mongodb/lib/core/uri_parser')
+const { description } = require('../package.json')
 
 const parseUri = (uriIn, opts) => {
     let host = ''
@@ -50,7 +51,7 @@ const run = ({ uri, id, key = null, verbose = false, waitToRetryTime = 2000 }) =
     const { host, port } = parseUri(uri, cliOptions)
     const meta = {
         id: 'mongo-node'
-        , description: 'kable mongodb node'
+        , description
     }
     const k = kable(id, { host, port, key, verbose, meta })
     return k.run(true).then(() => {
